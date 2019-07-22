59e936ed1459dd9d432cb2926a38fb9dfe2ee5ed
xixi <xixi@qq.com>
2019-07-22 21:28:54 +0800

    redux01 -02

---
+++ src-redux02/index.js
@@ -0,0 +1,13 @@
+//入口js
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import store from './redux/store'//都让store管理
+
+ReactDOM.render(<App store={store}/>,document.getElementById('root'))
+
+
+//绑定监视store内部状态数据改变的监听
+store.subscribe(() => {//重新渲染一边
+    ReactDOM.render(<App store={store}/>,document.getElementById('root'))
+})
---
+++ src-redux02/redux/reducer.js
@@ -0,0 +1,17 @@
+//真正管理状态数据的函数
+//作用:根据老的state 和  action, 产生新的state
+import {INCREMENT,DECREMENT} from './action-types'
+export default function count (state = 1,action){
+    switch(action.type){
+       case INCREMENT:
+           return state + action.number
+        case DECREMENT:
+            return state - action.number
+        default:    //产生初始状态值
+            return state
+    }
+    return
+}
+
+
+  
